733422511862abc89ea34108b4844436f9d0c963
hnhsnt <hannahgssantos@gmail.com>
2023-06-27 14:19:17 +0200

    created section-2 folder + exercises
    small exercises for a recap of section 1.

---
+++ small-exercises/section-2/twoStringsFunction.js
@@ -0,0 +1,9 @@
+// Challenge:
+// Create a function that logs out "Hi there, Linda!" when called.
+
+let name = "Linda";
+let message = "Hi there"
+
+function logMessage() {
+    console.log(message + ", " + name + "!");
+}
---
+++ small-exercises/section-2/variablesPractice.js
@@ -0,0 +1,14 @@
+// Challenge:
+// 1. Create two variables, firstName and lastName
+// 2. Concatenate the two variables into a third variable called fullName
+// 3. Log fullName to the console
+
+// 1.
+let firstName = "Mario";
+let lastName = "Rossi";
+
+// 2. 
+let fullName = firstName + " " + lastName;
+
+// 3.
+console.log(fullName);
